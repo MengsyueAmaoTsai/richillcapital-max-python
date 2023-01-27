5989d1985ce28e08ba5b236fdd4afe40a18dfc30
Megnsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-01-28 07:55:03 +0800

    feat(MaxClient): Add argument

--- richillcapital_max/client.py
+++ richillcapital_max/client.py
@@ -2,5 +2,6 @@
 
 class MaxClient(object):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, api_key: str, secret_key: str) -> None:
+        self._api_key: str = api_key
+        self._secret_key: str = secret_key
