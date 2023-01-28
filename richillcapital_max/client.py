5d17d948795ee0e6fa8b6b415fd02546ce340728
Megnsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-01-28 08:14:03 +0800

    Add class member REST_URL and WEBSOCKET_URL

--- richillcapital_max/client.py
+++ richillcapital_max/client.py
@@ -1,6 +1,11 @@
 
 
 class MaxClient(object):
+    """
+    """
+    
+    REST_URL = "https://max-api.maicoin.com"
+    WEBSOCKET_URL = "wss://max-stream.maicoin.com/ws"
 
     def __init__(self, api_key: str, secret_key: str) -> None:
         self._api_key: str = api_key
